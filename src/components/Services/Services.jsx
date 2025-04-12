fad9bae948ed6c44e62ef9ee00084357576d40e6
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-04-12 12:50:25 +0500

    Updating Service Cards

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -15,7 +15,7 @@ export function Services() {
       
       <div className="bg-white hover:cursor-pointer rounded-tl-2xl shadow-2xl card-container h-[max-content] rounded-br-2xl rounded-tr-4xl rounded-bl-4xl ml-[2%] "> 
           <div className="content  p-4 mt-6">
-            <h1 className="mb-22 text-xl capitalize hover:border-b-2 w-[max-content] duration-300 ">   🚚 Free Delivery
+            <h1 className="mb-22 text-[1.3rem] capitalize hover:border-b-2 w-[max-content] duration-300 ">   🚚 Free Delivery
    
             </h1>
             <p className="mb-6">
@@ -26,9 +26,9 @@ Let me know if you need any tweaks! 😊
              </p>
           </div>
         </div>
-        <div className="bg-white hover:cursor-pointer rounded-tl-2xl shadow-2xl card-container h-[max-content] rounded-br-2xl rounded-tr-4xl pb-6 rounded-bl-4xl ml-[2%] "> 
+        <div className="bg-white hover:cursor-pointer rounded-tl-2xl shadow-2xl card-container h-[max-content] rounded-br-2xl rounded-tr-4xl  rounded-bl-4xl ml-[2%] "> 
           <div className="content  p-4 mt-6 ">
-            <h1 className="mb-22 text-xl capitalize hover:border-b-2 w-[max-content] duration-300 ">  💧 Mineral Water
+            <h1 className="mb-22 text-[1.3rem] capitalize hover:border-b-2 w-[max-content] duration-300 ">  💧 Mineral Water
             </h1>
             <p>
             Stay refreshed with our pure and high-quality mineral water. Sourced from the finest springs, it’s packed with essential minerals to keep you hydrated and healthy every day.
@@ -39,7 +39,7 @@ Let me know if you need any adjustments! 😊
         </div>
         <div className="bg-white hover:cursor-pointer sm:mr-4 rounded-tl-2xl shadow-2xl h-[max-content] card-container rounded-br-2xl rounded-tr-4xl rounded-bl-4xl ml-[2%] ">
           <div className="content p-4  mt-10">
-            <h1 className="text-xl mb-18   capitalize hover:border-b-2 w-[max-content] duration-300 ">               💰 Retail Price
+            <h1 className="text-[1.3rem] mb-18   capitalize hover:border-b-2 w-[max-content] duration-300 ">               💰 Retail Price
             </h1>
             <p className="mb-6 ">
             Get the best quality products at unbeatable retail prices! We offer fair and affordable rates, ensuring you get great value for your money without compromising on quality. Shop smart and save more with us!
